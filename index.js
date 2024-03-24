e171ac4fba21e75e2b5d60cc820ca996937de97e
Syvhell Abad Umila <syvhellabadumila@gmail.com>
2024-03-24 18:37:28 +0800

    adding changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv"); // Require dotenv
+
 // Import Paths
 const userRoutes = require("./routes/user");
 const productRoutes = require("./routes/product");
@@ -12,15 +14,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Connecting to DB
-mongoose.connect(
-  "mongodb+srv://admin:admin@zuitt-bootcamp.xuatuvr.mongodb.net/Mophies?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-// Send Status to the console if connection is successful
+dotenv.config(); // Load environment variables from .env file
+
+// Connecting to DB using environment variable
+mongoose.connect(process.env.DB_CONNECT, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Send status to the console if connection is successful
 mongoose.connection.once("open", () =>
   console.log("Connected to the MongoDB Atlas")
 );
